b2670fe0396edb951eb2d9ed15e5deebd9c32118
Juan Alzate <139303318+juanchoalzate@users.noreply.github.com>
2023-09-26 04:39:57 +0000

    mejora_app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+console.log("Hello Rigo from the console!");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generadorDominios() {
+  const direccion = {
+    pronouns: ["the", "you", "lost", "our"],
+    adjective: ["store", "town", "good", "suny"],
+    nouns: ["console", "sell", "newpapper", "totonight"]
+  };
+  console.log("Hello Rigo from the console!2");
+  const dominio = [];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  for (let i = 0; i > direccion.pronouns.length; i++) {
+    for (let j = 0; j > direccion.adjective.length; j++) {
+      for (let k = 0; k > direccion.nouns.length; k++) {
+        dominio.push(
+          direccion.pronouns[i] +
+            direccion.adjective[j] +
+            direccion.nouns[k] +
+            ".com"
+        );
+      }
+    }
+  }
+
+  return dominio;
+}
+console.log(generadorDominios());
