3bc14c03b4d18939d04056d991a08fdf6fa96b21
Juan Alzate <139303318+juanchoalzate@users.noreply.github.com>
2023-09-28 01:07:37 +0000

    correxion_comparadores

--- src/app.js
+++ src/app.js
@@ -1,17 +1,15 @@
-console.log("Hello Rigo from the console!");
-
 function generadorDominios() {
   const direccion = {
     pronouns: ["the", "you", "lost", "our"],
     adjective: ["store", "town", "good", "suny"],
     nouns: ["console", "sell", "newpapper", "totonight"]
   };
-  console.log("Hello Rigo from the console!2");
+
   const dominio = [];
 
-  for (let i = 0; i > direccion.pronouns.length; i++) {
-    for (let j = 0; j > direccion.adjective.length; j++) {
-      for (let k = 0; k > direccion.nouns.length; k++) {
+  for (let i = 0; i < direccion.pronouns.length; i++) {
+    for (let j = 0; j < direccion.adjective.length; j++) {
+      for (let k = 0; k < direccion.nouns.length; k++) {
         dominio.push(
           direccion.pronouns[i] +
             direccion.adjective[j] +
@@ -21,7 +19,6 @@ function generadorDominios() {
       }
     }
   }
-
   return dominio;
 }
 console.log(generadorDominios());
